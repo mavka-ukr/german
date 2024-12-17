dcd16c60ca69c3b957d483224a459f308eea46bc
Davyd Kohut <davyd.kohut@gmail.com>
2024-12-17 23:02:52 +0200

    🧬

--- hermann.cpp
+++ hermann.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 
 extern "C" void print_long(long value) {
     printf("%ld", value);
@@ -23,4 +24,33 @@ extern "C" size_t read_file(const char *filename, char **out_address) {
 
     fclose(file);
     return size;
+}
+
+extern "C" unsigned char *mama_convert_utf32_to_utf8(unsigned int *utf32) {
+    unsigned long len = 0;
+    while (utf32[len] != 0) {
+        len++;
+    }
+    unsigned char *utf8 = (unsigned char *) malloc(len * 4 + 1);
+    unsigned char *utf8_start = utf8;
+    for (int i = 0; i < len; i++) {
+        unsigned int c = utf32[i];
+        if (c < 0x80) {
+            *utf8++ = c;
+        } else if (c < 0x800) {
+            *utf8++ = 0xC0 | c >> 6;
+            *utf8++ = 0x80 | (c & 0x3F);
+        } else if (c < 0x10000) {
+            *utf8++ = 0xE0 | c >> 12;
+            *utf8++ = 0x80 | (c >> 6 & 0x3F);
+            *utf8++ = 0x80 | (c & 0x3F);
+        } else {
+            *utf8++ = 0xF0 | c >> 18;
+            *utf8++ = 0x80 | (c >> 12 & 0x3F);
+            *utf8++ = 0x80 | (c >> 6 & 0x3F);
+            *utf8++ = 0x80 | (c & 0x3F);
+        }
+    }
+    *utf8 = 0;
+    return utf8_start;
 }
